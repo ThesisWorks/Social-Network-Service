2fd9d2be04b673340bba0b0f2f5540c79d3ae5b7
nhlee98 <e5slnh9876@gmail.com>
2023-11-09 17:43:05 +0900

    chore: 프로젝트 초기화

---
+++ src/main/java/com/meong/meongtwork/MeongtworkApplication.java
@@ -0,0 +1,13 @@
+package com.meong.meongtwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeongtworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeongtworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meong/meongtwork/MeongtworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meong.meongtwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeongtworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
